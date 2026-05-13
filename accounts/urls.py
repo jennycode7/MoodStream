03335bd530eb26faef153d2d83cb5edeef9ca5cb
Jennycode7 <jennessecosmas7@gmail.com>
2026-05-13 21:15:30 +0100

    refresh view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenRefreshView
 
 
 urlpatterns = [
     path('api/accounts/register/', views.RegisterView.as_view()),
     path('api/accounts/login/', views.LoginView.as_view()),
-    path('api/accounts/logout/', views.LogoutView.as_view())
+    path('api/accounts/logout/', views.LogoutView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view())
 ]
